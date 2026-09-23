f07196f7952898bf08734d264ccf7fab273c5cd0
Anoozkr1921 <anoozburnwal1921@gmail.com>
2026-09-24 00:12:49 +0530

    Updated Notices Page

--- src/data/notices.js
+++ src/data/notices.js
@@ -30,6 +30,11 @@ export const notices = [
     description: "Traditional campus celebration featuring cultural activities and community prayers to observe the auspicious occasion of Saraswati Puja.",
   },
   {
+    date: "12 january 2026",
+    title: "National Youth Festival",
+    description: "The nation chose two students of our campus to represent at the 29th National Youth Festival to be conducted from 10th to 12th January 2025 at Bharat Mandapam, NewDelhi.",
+  },
+  {
     date: "21 November 2025",
     title: "Unity March",
     description: "An institutional march organized by volunteers to promote communal harmony, national integration, and shared social peace.",
@@ -40,9 +45,49 @@ export const notices = [
     description: "A voluntary healthcare drive conducted in collaboration with local medical authorities to meet the requirements of regional blood banks.",
   },
   {
+    date: "12 October 2025",
+    title: "Viksit Bharat Marathon 2025",
+    description: "This event was organized under the national initiative Fit Youth For Viksit Bharat, with the aim of promoting health, fitness, and youth participation in the nation’s developmental journey towards a “Viksit Bharat” (Developed India).",
+  },
+  {
+    date: "25 September 2025",
+    title: "Essay on Women in Leadership: Catalysts for Change",
+    description: "The NSS unit of BIT Mesra organized an essay and poster competition on the theme “Women in Leadership: Catalysts for Change” to celebrate and promote women’s empowerment and leadership. Students expressed their ideas creatively, highlighting the vital role of women leaders in driving social progress, inspiring change, and breaking barriers.",
+  },
+  {
+    date: "9 October 2025",
+    title: "Consumer Awareness Drive",
+    description: "This programme emphasized the need to protect consumers from exploitation by ensuring they have access to information about products, prices, and safety, thereby empowering them to make informed decisions.",
+  },
+  {
+    date: "9 September 2025",
+    title: "Celebration of International Literacy Day",
+    description: "In this event, Various engaging activities, insightful discussions, and impactful awareness campaigns were conducted to inspire students and the community to promote education, eradicate illiteracy, bridge learning gaps, and build a more informed, empowered, and progressive society where knowledge becomes a tool for growth and social transformation.",
+  },
+  {
+    date: "2 September 2025",
+    title: "Awareness Drive on Indian Culture and Values",
+    description: "The NSS unit of BIT Mesra organized an awareness program on 2nd September 2025 to highlight the significance of Gurus and the richness of Indian culture. The programme featured engaging talks, interactive activities, and insightful discussions, through which students reflected on the vital role of teachers, traditional values, and cultural heritage in shaping individuals, guiding society, and fostering moral development.",
+  },
+  {
+    date: "29 August 2025",
+    title: "Visit to Old Age Home",
+    description: "In this event, NSS volunteers visited an Old Age Home, where they interacted warmly with the residents, sang songs with them, served food, and spent quality time listening to their experiences and stories."
+  },
+  {
+    date: "22 August 2025",
+    title: "The Spirit of Selfless Service",
+    description: "In this event, NSS volunteers participated in a meaningful activity based on the theme 'The Spirit of Selfless Service'."
+  },
+  {
     date: "15 August 2025",
     title: "Prabhat Pheri",
     description: "An early morning commemorative march held on Independence Day to honor the national legacy and engage community members.",
+  },
+  {
+    date: "8 August 2025",
+    title: "Awareness On Anti Ragging",
+    description: "An Anti-Ragging Awareness Programme. Students were encouraged to express their views against ragging through essay writing and poster making competitions."
   },
   {
     date: "21 June 2025",
