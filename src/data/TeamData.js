91a33327d81c6cb319cb6aab77630af492e2ee4e
Shade art <hamzabitmesra786@gmail.com>
2026-09-24 00:36:36 +0530

    insta links removed

--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -12,35 +12,30 @@ const teamData = {
             name: "Dr. O.P. Pandey",
             image: "/Pandey.png",
             linkedin: "https://www.linkedin.com/in/dr-om-prakash-b568a1119/?originalSubdomain=in",
-            instagram: "https://www.instagram.com/_omprakashpandey_?igsh=dnBrZGI3bmc3OTV3",
         },
         {
             role: "President",
             name: "Purushottam Kumar",
             image: "/teams/President.jpg",
             linkedin: "https://www.linkedin.com/in/purushottam405/",
-            instagram: "https://www.instagram.com/snap_speaks_/",
         },
         {
             role: "Joint President",
             name: "Udit Raj",
             image: "/teams/Joint_President.png",
             linkedin: "https://www.linkedin.com/in/udit-raj-7b2203245/",
-            instagram: "https://www.instagram.com/uditraj1098/",
         },
         {
             role: "Vice President",
             name: "Shruti Suman",
             image: "/teams/Vice_President.jpg",
             linkedin: "https://www.linkedin.com/in/shruti-suman-46a1b0315/",
-            instagram: "https://www.instagram.com/_shrutikeshri_/",
         },
         {
             role: "General Secretary",
             name: "Anand Kumar",
             image: "/teams/General_Sec.png",
             linkedin: "",
-            instagram: "",
         },
         {
            role: "Joint Secretary",
@@ -67,15 +62,12 @@ const teamData = {
             role: "Treasurer",
             name: "Harsh Kumar",
             image: "/teams/treasurer2.png",
-            linkedin: "",
-            instagram: "",
+
         },
         {
             role: "Joint Treasurer",
             name: "Ashish Singh",
             image: "/teams/Joint_treasurer.png",
-            linkedin: "",
-            instagram: "",
         },
     ],
 
@@ -86,14 +78,11 @@ const teamData = {
                 {
                     name: "Kushagra Bhatnagar",
                     image: "/teams/web cyber coordnator.png",
-                    linkedin: "https://www.linkedin.com/in/ayushman-agarwal-a08b9b160/",
-                    instagram: "",
+                   
                 },
                 {
                     name: "Arvind Paswan",
                     image: "/teams/web_incharge.jpg",
-                    linkedin: "",
-                    instagram: "https://www.instagram.com/strangr.dreamr?igsh=ZjR3N3Azb25kNHZt",
                 },
             ],
         },
@@ -103,7 +92,7 @@ const teamData = {
                 {
                     name: "Ashley Jain",
                     image: "/teams/coverage_ic.jpg",
-                    instagram: "",
+
                 },
             ],
         },
